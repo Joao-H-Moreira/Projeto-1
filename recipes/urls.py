ccda0cb217fd91c72a289d3b9d0fb6ce83ce2b94
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-08-05 19:46:16 -0300

    Eu faltei na aula 3 o manu me salvou rapidão

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
+from django.urls import path # Importa a função 'path' que define URLs.
+from . import views # Importa o arquivo views.py do próprio app.
 
 urlpatterns = [
-    path('',views.home, name='recipes_home'),
+path('', views.recipe_list, name='recipe_list'), # nova view para listar
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from .models import Recipe
 
-def home(request):
-
-    return HttpResponse("<h1> Bem-vindo à pàgina de Receitas!</h1> ")
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
