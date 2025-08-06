ae031821d72cd38d62148cfeededf5eb84b4dcac
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-08-05 21:43:44 -0300

    HTML pronto

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path # Importa a função 'path' que define URLs.
-from . import views # Importa o arquivo views.py do próprio app.
-
+from django.urls import path 
+from . import views
 urlpatterns = [
-path('', views.recipe_list, name='recipe_list'), # nova view para listar
+path('', views.recipe_list, name='recipe_list'), 
+path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
 ]
